799eb3409b994a48c173ef89037506dc164fc893
handekarai <handekarai.hk@gmail.com>
2022-06-15 01:14:50 +0300

    cell shadow added

--- CryptoApp/CryptoListModule/View/Cell/CryptoInfoCell.swift
+++ CryptoApp/CryptoListModule/View/Cell/CryptoInfoCell.swift
@@ -18,5 +18,26 @@ class CryptoInfoCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupUI()
+    }
+    
+    func setupUI(){
+        var cornerRadius: CGFloat = 5.0
+
+        // Apply rounded corners to contentView
+        contentView.layer.cornerRadius = cornerRadius
+        contentView.layer.masksToBounds = true
+                
+        // Set masks to bounds to false to avoid the shadow
+        // from being clipped to the corner radius
+        layer.cornerRadius = cornerRadius
+        layer.masksToBounds = false
+                
+        // Apply a shadow
+        layer.shadowRadius = 8.0
+        layer.shadowOpacity = 0.10
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 5)
+           
     }
 }
